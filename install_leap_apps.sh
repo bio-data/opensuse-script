4ae71cbf84cd15bd83639b8452f1e02b5990ec01
Vinícius Barros Rodrigues <viniciusbrbio@gmail.com>
2017-01-20 10:54:20 -0200

    Update install_leap_apps.sh

--- install_leap_apps.sh
+++ install_leap_apps.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# For openSUSE Leap 42.2
+# For openSUSE Leap 42.2 and use at your own risk!
+# Vinícius Barros Rodrigues, for sugestions: viniciusbrbio@gmail.com
+# If you want avoid installing some repos add hashtag in the begin of the line.
 
 echo "Update and dist-upgrade"
 sudo zypper -n up
@@ -16,10 +18,10 @@ sudo zypper -n install ffmpeg lame gstreamer-plugins-bad gstreamer-plugins-ugly 
 sudo zypper dist-upgrade
 
 echo "Installing apps"
-sudo zypper -n install inkscape gimp lollypop bleachbit unetbootin wine xournal kile goldendict pdfchain uget steam octave vlc R-base git kdenlive audacity simplescreenrecorder gpsbabel virtualbox openshot breeze keepassx 
+sudo zypper -n install inkscape gimp lollypop bleachbit unetbootin wine xournal kile goldendict pdfchain uget steam octave vlc R-base git kdenlive audacity simplescreenrecorder gpsbabel virtualbox openshot breeze keepassx meld
 
 echo "Some latex and R packages"
-sudo zypper install cmake automake texlive-subfloat zlibrary ghc-zlib zlib-devel libopenssl-devel libssh2-devel libcurl-devel libconfig9 libconfig++9 libconfig-devel gcc-c++
+sudo zypper install cmake automake texlive-subfloat zlibrary ghc-zlib zlib-devel libopenssl-devel libssh2-devel libcurl-devel libconfig9 libconfig++9 libconfig-devel gcc-c++ typelib-1_0-GtkVnc-1_0 typelib-1_0-GtkVnc-2_0 libgtk-vnc-1_0-0 libgtk-vnc-2_0-0
 
 sudo zypper -n ar -f http://download.opensuse.org/repositories/home:/awissu/openSUSE_Leap_42.2/home:awissu.repo
 
@@ -39,7 +41,7 @@ sudo zypper -n refresh
 
 sudo zypper -n up
 
-sudo zypper install brackets GitKraken PlayOnLinux viking jabref skype
+sudo zypper install brackets GitKraken PlayOnLinux viking jabref skype 
 
 wget https://download1.rstudio.org/rstudio-1.0.44-x86_64.rpm
 
