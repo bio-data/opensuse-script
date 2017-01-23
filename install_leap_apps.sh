73d14e29ed5ab12cf6fc2525af2c2f4b832e9963
Vinícius Barros Rodrigues <viniciusbrbio@gmail.com>
2017-01-23 12:50:28 -0200

    Update install_leap_apps.sh

--- install_leap_apps.sh
+++ install_leap_apps.sh
@@ -1,27 +1,46 @@
 #!/bin/bash
+
+echo "+-----------------------------------------------------+"
+echo "|    openSUSE 42.2 GNOME apps installation script     |"
+echo "+-----------------------------------------------------+"
+
 # For openSUSE Leap 42.2 and use at your own risk!
 # Vinícius Barros Rodrigues, for sugestions: viniciusbrbio@gmail.com
 # If you want avoid installing some repos add hashtag in the begin of the line.
 
+echo "+------------------------------------------------------------------+"
 echo "Update and dist-upgrade"
+echo "+------------------------------------------------------------------+"
 sudo zypper -n up
 sudo zypper -n dist-upgrade
 
+echo "+------------------------------------------------------------------+"
 echo "Packman and Libdvdcss repos for multimedia"
+echo "+------------------------------------------------------------------+"
 sudo zypper -n ar -f http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Leap_42.2/ packman
 sudo zypper -n ar -f http://opensuse-guide.org/repo/openSUSE_Leap_42.2/ libdvdcss
 sudo zypper --gpg-auto-import-keys ref
 sudo zypper up
 
+echo "+------------------------------------------------------------------+"
 echo "Installing multimedia packages"
+echo "+------------------------------------------------------------------+"
 sudo zypper -n install ffmpeg lame gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-plugins-ugly-orig-addon gstreamer-plugins-libav libdvdcss2 --auto-agree-with-licenses
 sudo zypper dist-upgrade
 
+echo "+------------------------------------------------------------------+"
 echo "Installing apps"
+echo "+------------------------------------------------------------------+"
 sudo zypper -n install inkscape gimp lollypop bleachbit unetbootin evince-plugin-djvudocument evince-plugin-comicsdocument evince-plugin-xpsdocument evince-plugin-tiffdocument evince-plugin-dvidocument wine xournal kile goldendict pdfchain uget steam octave vlc R-base git kdenlive audacity simplescreenrecorder gpsbabel virtualbox openshot breeze keepassx meld
 
+echo "+------------------------------------------------------------------+"
 echo "Some latex and R packages"
+echo "+------------------------------------------------------------------+"
 sudo zypper install cmake automake texlive-subfloat zlibrary ghc-zlib zlib-devel libopenssl-devel libssh2-devel libcurl-devel libconfig9 libconfig++9 libconfig-devel gcc-c++ typelib-1_0-GtkVnc-1_0 typelib-1_0-GtkVnc-2_0 libgtk-vnc-1_0-0 libgtk-vnc-2_0-0
+
+echo "+------------------------------------------------------------------+"
+echo "Add repos"
+echo "+------------------------------------------------------------------+"
 
 sudo zypper -n ar -f http://download.opensuse.org/repositories/home:/awissu/openSUSE_Leap_42.2/home:awissu.repo
 
@@ -41,6 +60,10 @@ sudo zypper -n refresh
 
 sudo zypper -n up
 
+echo "+------------------------------------------------------------------+"
+echo "Installing apps"
+echo "+------------------------------------------------------------------+"
+
 sudo zypper install brackets GitKraken PlayOnLinux viking jabref skype 
 
 wget https://download1.rstudio.org/rstudio-1.0.44-x86_64.rpm
@@ -53,14 +76,20 @@ sudo zypper -n dist-upgrade
 
 sudo zypper -n dist-upgrade --from packman
 
+echo "+------------------------------------------------------------------+"
 echo "Finishing"
+echo "+------------------------------------------------------------------+"
 sudo zypper -n verify
 sudo zypper -n clean
 
+echo "+------------------------------------------------------------------+"
 echo "Removing trash"
+echo "+------------------------------------------------------------------+"
 sudo zypper -n remove gnome-contacts gnome-dictionary gnome-weather empathy okular polari liferea konqueror gnome-documents gnome-music gnome-maps evolution shotwell aisleriot iagno xboard gnuchess quadrapassel gnome-clocks gnome-sudoku swell-foop lightsoff gnome-mines gnome-mahjongg bijiben gnome-software nautilus-sendto evolution-data-server
 
+echo "+------------------------------------------------------------------+"
 echo "Reboot?"
+echo "+------------------------------------------------------------------+"
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) sudo reboot;;
