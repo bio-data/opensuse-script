fe790c363609361d76fac7433d02abcbffb212a2
Vinícius Barros Rodrigues <viniciusbrbio@gmail.com>
2017-01-30 22:51:21 -0200

    Update install_leap_apps.sh

--- install_leap_apps.sh
+++ install_leap_apps.sh
@@ -6,7 +6,7 @@ echo "+-----------------------------------------------------+"
 
 # For openSUSE Leap 42.2 and use at your own risk!
 # Vinícius Barros Rodrigues, for sugestions: viniciusbrbio@gmail.com
-# If you want avoid installing some repos add hashtag in the begin of the line.
+# If you want avoid installing some stuffs add hashtag in the lines.
 
 echo "+------------------------------------------------------------------+"
 echo "Update and dist-upgrade"
@@ -15,7 +15,7 @@ sudo zypper -n up
 sudo zypper -n dist-upgrade
 
 echo "+------------------------------------------------------------------+"
-echo "Packman and Libdvdcss repos for multimedia"
+echo "Packman and Libdvdcss repos for Multimedia"
 echo "+------------------------------------------------------------------+"
 sudo zypper -n ar -f http://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Leap_42.2/ packman
 sudo zypper -n ar -f http://opensuse-guide.org/repo/openSUSE_Leap_42.2/ libdvdcss
@@ -23,52 +23,88 @@ sudo zypper --gpg-auto-import-keys ref
 sudo zypper up
 
 echo "+------------------------------------------------------------------+"
-echo "Installing multimedia packages"
+echo "Installing Multimedia Packages"
 echo "+------------------------------------------------------------------+"
 sudo zypper -n install ffmpeg lame gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-plugins-ugly-orig-addon gstreamer-plugins-libav libdvdcss2 --auto-agree-with-licenses
 sudo zypper dist-upgrade
 
 echo "+------------------------------------------------------------------+"
-echo "Installing apps"
+echo "Installing Apps"
 echo "+------------------------------------------------------------------+"
-sudo zypper -n install inkscape gimp lollypop bleachbit unetbootin evince-plugin-djvudocument evince-plugin-comicsdocument evince-plugin-xpsdocument evince-plugin-tiffdocument evince-plugin-dvidocument wine xournal kile goldendict pdfchain uget steam octave vlc R-base git kdenlive audacity simplescreenrecorder gpsbabel virtualbox openshot breeze keepassx meld
-
-echo "+------------------------------------------------------------------+"
-echo "Some latex and R packages"
-echo "+------------------------------------------------------------------+"
-sudo zypper install cmake automake texlive-subfloat zlibrary ghc-zlib zlib-devel libopenssl-devel libssh2-devel libcurl-devel libconfig9 libconfig++9 libconfig-devel gcc-c++ typelib-1_0-GtkVnc-1_0 typelib-1_0-GtkVnc-2_0 libgtk-vnc-1_0-0 libgtk-vnc-2_0-0
+sudo zypper -n install inkscape gimp lollypop bleachbit unetbootin evince-plugin-djvudocument evince-plugin-comicsdocument evince-plugin-xpsdocument evince-plugin-tiffdocument evince-plugin-dvidocument wine xournal kile goldendict pdfchain uget steam octave vlc R-base git kdenlive audacity simplescreenrecorder gpsbabel virtualbox openshot breeze keepassx meld gpodder geany latexila
 
 echo "+------------------------------------------------------------------+"
-echo "Add repos"
+echo "Some packages"
+echo "+------------------------------------------------------------------+"
+sudo zypper install cmake automake texlive-subfloat texlive-wrapfig zlibrary ghc-zlib zlib-devel libopenssl-devel libssh2-devel libcurl-devel libconfig9 libconfig++9 libconfig-devel gcc-c++ typelib-1_0-GtkVnc-1_0 typelib-1_0-GtkVnc-2_0 libgtk-vnc-1_0-0 libgtk-vnc-2_0-0 gtk-devel libxml2-devel gtk-devel gnome-doc-utils gnome-doc-utils-devel libgexiv2-devel libgps22
+
+echo "+------------------------------------------------------------------+"
+echo "Electron Apps"
 echo "+------------------------------------------------------------------+"
 
-sudo zypper -n ar -f http://download.opensuse.org/repositories/home:/awissu/openSUSE_Leap_42.2/home:awissu.repo
+mkdir ElectronsApps
 
-sudo zypper -n ar -f http://download.opensuse.org/repositories/home:/luke_nukem/openSUSE_Leap_42.2/home:luke_nukem.repo
+echo "GitKraken"
+cd ElectronsApps
+wget https://release.gitkraken.com/linux/gitkraken-amd64.tar.gz
+sudo tar -vzxf gitkraken-amd64.tar.gz
+rm gitkraken-amd64.tar.gz
 
-sudo zypper -n ar -f http://download.opensuse.org/repositories/games:/tools/openSUSE_Leap_42.2/games:tools.repo
+echo "Franz"
+mkdir Franz
+wget https://github.com/meetfranz/franz-app/releases/download/4.0.4/Franz-linux-x64-4.0.4.tgz
+sudo tar -vzxf Franz-linux-x64-4.0.4.tgz
+rm Franz-linux-x64-4.0.4.tgz
+cd ..
 
-sudo zypper -n ar -f http://download.opensuse.org/repositories/Application:/Geo/openSUSE_Leap_42.2/Application:Geo.repo
+echo "Manageyum"
+wget https://github.com/Revmakx/manageyum/releases/download/V1.1.1/Manageyum-1.1.1.x64.tar.gz
+sudo tar -vzxf Manageyum-1.1.1.x64.tar.gz
+rm Manageyum-1.1.1.x64.tar.gz
 
-sudo zypper -n ar -f http://download.opensuse.org/repositories/home:/cornell_vrdc/openSUSE_Leap_42.2/home:cornell_vrdc.repo
+echo "ClipGrab
+wget https://download.clipgrab.org/clipgrab-3.6.2.tar.bz2
+sudo tar -jxvf clipgrab-3.6.2.tar.bz2
+rm clipgrab-3.6.2.tar.bz2
 
-sudo zypper --gpg-auto-import-keys ref
+echo "Stremio"
+wget http://dl.strem.io/Stremio3.6.5.linux.tar.gz
+sudo tar -vzxf Stremio3.6.5.linux.tar.gz
+rm clipgrab-3.6.2.tar.bz2
+
+echo "JabRef"
+wget https://downloads.sourceforge.net/project/jabref/v3.8.2/JabRef-3.8.2.jar
+# java -jar JabRef-3.8.2.jar comando pra abrir
+
+cd ..
+
+echo "yEd"
+wget http://www.yworks.com/resources/yed/demo/yEd-3.16.2.1_64-bit_setup.sh
+chmod +x yEd-3.16.2.1_64-bit_setup.sh
+sh yEd-3.16.2.1_64-bit_setup.sh
+sudo rm yEd-3.16.2.1_64-bit_setup.sh
+
+echo "Viking GPS"
+wget https://downloads.sourceforge.net/project/viking/viking-1.6.2.tar.bz2
+sudo tar -jxvf viking-1.6.2.tar.bz2
+cd viking-1.6.2
+sudo ./configure --disable-realtime-gps-tracking --disable-magic --disable-mbtiles --disable-mapnik
+sudo make
+sudo make install
+
+echo "Rstudio"
+wget https://download1.rstudio.org/rstudio-1.0.136-x86_64.rpm
+sudo zypper -n install rstudio-1.0.136-x86_64.rpm
+sudo rm rstudio-1.0.136-x86_64.rpm
+
+#echo "Brackets"
+#wget https://github.com/adobe/brackets/archive/release-1.8.tar.gz
+#sudo tar -vzxf release-1.8.tar.gz
+#sudo rm release-1.8.tar.gz
 
 sudo zypper -n refresh
 
 sudo zypper -n up
-
-echo "+------------------------------------------------------------------+"
-echo "Installing apps"
-echo "+------------------------------------------------------------------+"
-
-sudo zypper install brackets GitKraken PlayOnLinux viking jabref
-
-wget https://download1.rstudio.org/rstudio-1.0.136-x86_64.rpm
-
-sudo zypper -n install rstudio-1.0.136-x86_64.rpm
-
-sudo rm rstudio-1.0.136-x86_64.rpm
 
 sudo zypper -n dist-upgrade
 
